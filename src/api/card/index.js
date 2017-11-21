50569f648e110c57e543b5b162742c7440f44557
Hasitha Seneviratne <hasitha.bigwig@gmail.com>
2017-11-21 22:42:15 +0530

    Bug fix - Card API get by id

--- src/api/card/index.js
+++ src/api/card/index.js
@@ -17,18 +17,18 @@ router.get('/', function(req, res, next) {
 
 /* GET SINGLE CARD BY ID */
 router.get('/:id', function(req, res, next) {
-  Card.findOne({card_number: req.params.id}, function (err, post) {
-    if (err) {
+  Card.find({card_number: req.params.id}, function (err, results) {
+    if (err || results == null) {
       res.status(400).json({status: 400, error: "invalid id - " + req.params.id});
       return next(err);
     }
-    res.json({status: 200, content: post});
+    res.json({status: 200, content: results});
   });
 });
 
 /* SAVE CARD */
 router.post('/', function(req, res, next) {
-  Card.create(req.body, function (err, post) {
+  Card.create(req.body, function (err, results) {
     if (err) {
       for (var prop in err.errors) {
         if (err.errors.hasOwnProperty(prop)) {
@@ -38,28 +38,28 @@ router.post('/', function(req, res, next) {
       res.status(400).json({error: errorMsg});
       return next(err);
     }
-    res.json({status: 200, content: post});
+    res.json({status: 200, content: results});
   });
 });
 
 /* UPDATE CARD */
 router.put('/:id', function(req, res, next) {
-  Card.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+  Card.findByIdAndUpdate(req.params.id, req.body, function (err, results) {
     if (err) {
       res.status(232).json(err);
       // res.status(400).json({status: 400, error: "invalid id - " + req.params.id});
       return next(err);
     }
-    res.json({status: 200, content: post});
+    res.json({status: 200, content: results});
   });
 });
 
 
 /* DELETE CARD */
 router.delete('/:id', function(req, res, next) {
-  Card.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+  Card.findByIdAndRemove(req.params.id, req.body, function (err, results) {
     if (err) return next(err);
-    res.json({status: 200, content: post});
+    res.json({status: 200, content: results});
   });
 });
 
